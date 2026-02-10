a199b3b121490a6f4de5203eb0e2e3edcb6282da
phurbasherpa13 <13phurbasherpa@gmail.com>
2026-02-10 15:45:19 +0545

    def init

--- secure_chat.py
+++ secure_chat.py
@@ -40,4 +40,12 @@ class SecurityEngine:
 
 
 class ChatCore:
-    
+    def __init__(self, log_callback, connected_callback, disconnected_callback):
+        self.log = log_callback
+        self.on_connected = connected_callback
+        self.on_disconnected = disconnected_callback
+        self.websocket = None
+        self.loop = None
+        self.security = None
+        self.running = False
+
